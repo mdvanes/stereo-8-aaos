b009838ae961e898d7e43731b081ffe156a3fb4a
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-06-26 10:55:45 +0200

    Add subsonic

--- components/StationButton/StationButton.tsx
+++ components/StationButton/StationButton.tsx
@@ -1,9 +1,49 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Button } from "react-native";
 import { Audio } from "expo-av";
+// @ts-expect-error
+import md5 from "md5";
 import { Text } from "../Themed";
+import settings from "./settings.json";
 
 const channelUrl = `https://icecast.omroep.nl/radio2-bb-mp3`;
+
+const getMovies = async () => {
+  try {
+    const response = await fetch("https://reactnative.dev/movies.json");
+    const json = await response.json();
+    console.log(json);
+    //  setData(json.movies);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    //  setLoading(false);
+  }
+};
+
+const API_DOMAIN = settings.subsonic.domain;
+const API_USER = settings.subsonic.user;
+const API_SALT = settings.subsonic.salt;
+const API_TOKEN = md5(settings.subsonic.password + API_SALT);
+const API_CONFIG = `?u=${API_USER}&t=${API_TOKEN}&s=${API_SALT}&v=1.16.0&c=Stereo8&f=json`;
+
+const getAPI = (method: string, option = "") =>
+  API_DOMAIN + method + API_CONFIG + option;
+
+const getSubsonic = async () => {
+  try {
+    const response = await fetch(getAPI("getNowPlaying"));
+    const json = await response.json();
+    // console.log(json);
+    const { artist, title } = json["subsonic-response"].nowPlaying.entry[0];
+    console.log(`Now playing: ${artist} - ${title}`);
+    //  setData(json.movies);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    //  setLoading(false);
+  }
+};
 
 // TODO extract channelUrl and label to props
 export const StationButton = () => {
@@ -22,6 +62,8 @@ export const StationButton = () => {
 
   useEffect(() => {
     init();
+    getMovies();
+    getSubsonic();
   }, []);
 
   const onToggle = async () => {
