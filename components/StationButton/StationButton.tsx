0d868d38dd104f325f52087df67c8976fe6c5116
mdvanes <4253562+mdvanes@users.noreply.github.com>
2022-06-27 15:14:40 +0200

    Add radio meta data

--- components/StationButton/StationButton.tsx
+++ components/StationButton/StationButton.tsx
@@ -1,24 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Audio } from "expo-av";
 import { ListItemButton } from "./ListItemButton";
+// @ts-expect-error
+import MediaMeta from "react-native-media-meta";
 
 const channelUrl = `https://icecast.omroep.nl/radio2-bb-mp3`;
 
-const getMovies = async () => {
+// const getMovies = async () => {
+//   try {
+//     const response = await fetch("https://reactnative.dev/movies.json");
+//     const json = await response.json();
+//     console.log(json);
+//     //  setData(json.movies);
+//   } catch (error) {
+//     console.error(error);
+//   } finally {
+//     //  setLoading(false);
+//   }
+// };
+
+const getMediaMeta = async (): Promise<string> => {
   try {
-    const response = await fetch("https://reactnative.dev/movies.json");
-    const json = await response.json();
-    console.log(json);
-    //  setData(json.movies);
-  } catch (error) {
-    console.error(error);
-  } finally {
-    //  setLoading(false);
+    const metadata = await MediaMeta.get(channelUrl);
+    console.log(metadata);
+    return metadata;
+  } catch (err) {
+    console.error(err);
+    return "error";
   }
+
+  // .then((metadata: any) => console.log(metadata))
+  // .catch((err: any) => console.error(err));
 };
 
 // TODO extract channelUrl and label to props
 export const StationButton = () => {
+  const [meta, setMeta] = useState<string>();
   const [isPlaying, setIsPlaying] = useState(false);
   const [pbo, setPbo] = useState<Audio.Sound | null>(null);
 
@@ -29,11 +46,13 @@ export const StationButton = () => {
       { shouldPlay: false }
     );
     setPbo(playbackObject);
+    const newMeta = await getMediaMeta();
+    setMeta(newMeta);
   };
 
   useEffect(() => {
     init();
-    getMovies();
+    // getMovies();
   }, []);
 
   const onToggle = async () => {
@@ -49,7 +68,7 @@ export const StationButton = () => {
 
   return (
     <ListItemButton
-      text="NPO Radio 2"
+      text={meta || "NPO Radio 2"}
       title={isPlaying ? "pause" : "play"}
       onClick={onToggle}
     />
