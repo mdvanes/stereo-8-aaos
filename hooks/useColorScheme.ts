d83d25ca490c107ed343c7f586b6c159c4e277d4
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-01-22 15:59:10 +0100

    force dark mode more forcy

--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -4,5 +4,6 @@ import { ColorSchemeName, useColorScheme as _useColorScheme } from 'react-native
 // type suggests that it can be null. This will not happen in practice, so this
 // makes it a bit easier to work with.
 export default function useColorScheme(): NonNullable<ColorSchemeName> {
-  return _useColorScheme() as NonNullable<ColorSchemeName>;
+  return "dark"; // Force dark mode this way, userInterfaceStyle is ignored somehow. See https://docs.expo.dev/guides/color-schemes/
+  // return _useColorScheme() as NonNullable<ColorSchemeName>;
 }
