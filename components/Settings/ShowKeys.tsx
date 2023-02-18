197bd21b3aef4b03fb8a5c39758f9eab509e7cde
mdvanes <4253562+mdvanes@users.noreply.github.com>
2023-02-18 16:36:12 +0100

    add fwd prev and pause on hardware keys

--- components/Settings/ShowKeys.tsx
+++ components/Settings/ShowKeys.tsx
@@ -1,19 +1,84 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useContext, useEffect, useState } from "react";
 import KeyEvent from "react-native-keyevent";
+import { PlayContext } from "../context/play-context";
+import { useGetNextSong } from "../SoundWrapper/useGetNextSong";
+import { useGetPreviousSong } from "../SoundWrapper/useGetPreviousSong";
 import { View, Text } from "../Themed";
 
+type OnKeyDownEvent = { keyCode: number; action: unknown; pressedKey: unknown };
+
+// TODO works in car and does not override volume up/down like in emulator Keycode 88 back 85 play 87 fwd
 export const ShowKeys: FC = () => {
   const [log, setLog] = useState("");
+  const context = useContext(PlayContext);
+  const { getNextSong } = useGetNextSong();
+  const { getPreviousSong } = useGetPreviousSong();
+
+  const previous = () => {
+    const previousSong = getPreviousSong();
+    if (previousSong) {
+      context.setStartSongId(previousSong.id);
+    }
+  };
+
+  const next = () => {
+    const nextSong = getNextSong();
+    if (nextSong) {
+      context.setStartSongId(nextSong.id);
+    }
+  };
+
+  const pause = async () => {
+    if (context.pbo) {
+      console.log("pauseAsync", context);
+      // dispatch(setIsRadioPlaying(false));
+      await context.pbo.pauseAsync();
+      context.setIsPlaying(false);
+    }
+  };
+
+  const keyCodeMap = {
+    21: {
+      label: "previous",
+      fn: previous,
+    },
+    88: {
+      label: "previous",
+      fn: previous,
+    },
+    20: {
+      label: "pause",
+      fn: pause,
+    },
+    85: {
+      label: "pause",
+      fn: pause,
+    },
+    22: {
+      label: "next",
+      fn: next,
+    },
+    87: {
+      label: "next",
+      fn: next,
+    },
+  } as const;
 
   useEffect(() => {
-    KeyEvent.onKeyDownListener((keyEvent: any) => {
+    KeyEvent.onKeyDownListener((keyEvent: OnKeyDownEvent) => {
+      // TODO fix type
+      const action: { label: string; fn: () => void } | undefined =
+        // @ts-expect-error
+        keyCodeMap[keyEvent.keyCode];
+
       // TODO does not work on web, but does work on emulator. E.g. arrow down:  LOG  onKeyDown keyCode: 20 LOG  Action: 0 LOG  Key:
-      console.log(`onKeyDown keyCode: ${keyEvent.keyCode}`);
-      console.log(`Action: ${keyEvent.action}`);
-      console.log(`Key: ${keyEvent.pressedKey}`);
-      setLog(
-        `keycode=${keyEvent.keyCode} action=${keyEvent.action} key=${keyEvent.pressedKey}`
-      );
+      const msg = `keycode=${keyEvent.keyCode} action=${keyEvent.action} key=${keyEvent.pressedKey} action=${action?.label}`;
+      console.log(msg);
+      setLog(msg);
+
+      if (action) {
+        action.fn();
+      }
     });
 
     return () => {
---
+++ components/SoundWrapper/useGetPreviousSong.tsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import { MusicDirectorySong } from "../../types";
+import { PlayContext } from "../context/play-context";
+
+export const useGetPreviousSong = () => {
+  const context = useContext(PlayContext);
+
+  const getPreviousSong = (): MusicDirectorySong | null => {
+    console.log(context.startSongId, context.queue);
+    if (context.queue) {
+      const prevIndex = context.queue?.findIndex(
+        (queueItem) => queueItem.id === context.startSongId
+      );
+
+      const isFirst = prevIndex <= 0;
+
+      if (isFirst) {
+        // Wrap around to last song
+        return context.queue[context.queue.length - 1];
+      }
+
+      const prevSong = context.queue.slice(prevIndex - 1)[0];
+      console.log("previous song:", prevSong);
+      return prevSong;
+    }
+    return null;
+  };
+
+  return { getPreviousSong };
+};
